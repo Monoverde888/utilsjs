11c857e78a80b12ac4d7330a6c258cc19f3a41f7
Monoverde <minecraftersov@gmail.com>
2020-09-05 14:08:18 -0400

    pe

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+/** 
+ * This generates a random number with math floor and math random
+ * @param {number} min - The minimum number to generate
+ * @param {number} max - The maximum number to generate
+ */
 exports.randomNumber = function(min, max) {
-    if(!min) throw new TypeError("No has puesto el minimo de numeros!")
-    if(!max) throw new TypeError("No has puesto el maximo de numeros!")
+    if(!min) throw new TypeError("You havent put the minimum number to generate!")
+    if(!max) throw new TypeError("You havent put the maximum number to generate!")
     return Math.floor(Math.random() * (max - min) + min) 
   }
   
