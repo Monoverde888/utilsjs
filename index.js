639d8fd0eae41602f6798d0d5635b927931a300b
Monoverde <minecraftersov@gmail.com>
2020-09-05 15:07:56 -0400

    add random letter feature

--- index.js
+++ index.js
@@ -4,8 +4,22 @@
  * @param {number} max - The maximum number to generate
  */
 exports.randomNumber = function(min, max) {
-    if(!min) throw new TypeError("You havent put the minimum number to generate!")
-    if(!max) throw new TypeError("You havent put the maximum number to generate!")
+    if(!min) throw new TypeError("You havent put the minimum number to generate!");
+    if(!max) throw new TypeError("You havent put the maximum number to generate!");
     return Math.floor(Math.random() * (max - min) + min) 
   }
   
+/**
+ * This generates a random number or letter 
+ * Usage: util.randomLetter("ABCDEFGHIJKLMNOPQRSTUVWQYZ")
+ * @param {string} letters 
+ */
+exports.randomLetter = function(letters) {
+    var result           = '';
+    var characters       = letters;
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
