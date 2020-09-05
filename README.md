3291a4ea37b56d009f073abd1887eb9c1d702e91
Monoverde <minecraftersov@gmail.com>
2020-09-04 23:43:35 -0400

    removido code ya que tiene errores

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![GitHub package.json version](https://img.shields.io/github/package-json/v/Monoverde888/utilsjs?color=gree&label=Version&logo=NPM&style=flat-square)
 # utilsjs
-NPM package to make programmers life easiest
+NPM package to make programmers life easiest  
+# [Package](https://www.npmjs.com/package/@monoverde/utilsjs)
--- index.js
+++ index.js
@@ -3,27 +3,4 @@ exports.randomNumber = function(min, max) {
     if(!max) throw new TypeError("No has puesto el maximo de numeros!")
     return Math.floor(Math.random() * (max - min) + min) 
   }
-  exports.code = function(lang, code) {
-      lang:
-       "asciidoc"
-       "autohotkey"
-       "bash"
-       "coffeescript"
-       "cpp"
-       "cs"
-       "css"
-       "diff"
-       "fix"
-       "glsl"
-       "ini"
-       "json"
-       "md"
-       "ml"
-       "prolog"
-       "py"
-       "tex"
-       "xl"
-       "xml"
-      code:
-      String
-  }
+  
